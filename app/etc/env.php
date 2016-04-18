76cb23eeacc2ca517a367c26115ceb6b7a72d45a
Edward Brosens <brosens@qelp.com>
2016-04-18 10:01:53 +0200

    redesign initial commit

---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'install' => 
+  array (
+    'date' => 'Sun, 17 Apr 2016 14:46:01 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => '2115b72b294abdb30b6fa63323462a40',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => '127.0.0.1',
+        'dbname' => 'ambacht',
+        'username' => 'root',
+        'password' => 'root',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+);
---
+++ pub/static/frontend/Venustheme/yume/nl_NL/js/ambacht.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(docReady);
+function docReady() {
+    
+    jQuery('.header.content').prepend('<img src="http://lorempixel.com/1024/500/" />');
+    
+    
+    
+    
+    
+}
